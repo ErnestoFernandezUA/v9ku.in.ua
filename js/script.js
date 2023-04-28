5fc180b9d96f0e766c0c219e48560ce0828777ad
E Fernandez <ernesto.fernandez.1980@gmail.com>
2023-04-28 17:31:35 +0300

    add slider to navigation

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    arrows: false,
+    dots: true,
+    // slidesToShow: 4,
+    // slidesToScroll: 1,
+    // speed: 1000,
+    infinite: false,
+    // initialSlide: 0,
+    centerMode: false,
+    variableWidth: true,
+    // draggable: false,
+    touchThreshold: 10,
+  });
+});
