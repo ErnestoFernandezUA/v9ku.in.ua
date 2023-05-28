82a935514df00b89606493bbbaa6f1fe26ea0ef6
E Fernandez <ernesto.fernandez.1980@gmail.com>
2023-05-28 23:57:22 +0300

    swiper photo working

--- js/swiper-photo.js
+++ js/swiper-photo.js
@@ -1,35 +1,34 @@
 new Swiper('.swiper-photo', {
   enabled: true,
-  slidesPerView: 'auto',
-  spaceBetween: 100,
+  slidesPerView: 1,
+  spaceBetween: 20,
   speed: 400,
   // height: 200,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  centeredSlides: true,
-  // scrollbar: {
-  //   el: '.swiper-scrollbar--photo',
-  //   dragSize: 70,
-  //   hide: true,
-  //   // grid: {
-  //   //   rows: 3,
-  //   // },
-  //   // mousewheel: {
-  //   //   forceToAxis: true,
-  //   // },
-  //   },
-  // breakpoints: {
-  //   640: {
-  //     slidesPerView: 2,
-  //   },
-  //   1024: {
-  //     slidesPerView: 3,
-  //   },
-  //   1260: {
-  //     slidesPerView: 'auto',
-  //     height: 200,
-  //   }
-  // },
+  // centeredSlides: true,
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    dragSize: 70,
+    // hide: true,
+    // grid: {
+    //   rows: 3,
+    // },
+    // mousewheel: {
+    //   forceToAxis: true,
+    // },
+    },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    1260: {
+      slidesPerView: 1,
+    }
+  },
 });
