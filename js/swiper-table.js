4fd88f7239998e64de28a8eef4472b63e68f95a9
E Fernandez <ernesto.fernandez.1980@gmail.com>
2023-06-08 12:18:37 +0300

    fix swiper-table

--- js/swiper-table.js
+++ js/swiper-table.js
@@ -1,26 +1,23 @@
-// const swipersTable = document.querySelectorAll('.swiper--table');
+const swipersTable = document.querySelector('.swiper-table');
 
-// console.log(swipersTable.length);
-
-// if (!swipersLeagues.length) {
-//   console.error('class .swiper--table is not found');
-// }
-
-new Swiper('.swiper-table', {
-  enabled: Boolean(window.innerWidth < breakpointSlider),
-  slidesPerView: 'auto',
-  spaceBetween: 5,
-  speed: 1000,
-  allowTouchMove: true,
-  scrollbar: {
-    el: '.swiper-scrollbar',
-    dragSize: 70,
-    // hide: true,
-  },
-
-  on: {
-    slideChange: function () {
-      console.log('slide changed');
+if (!swipersTable) {
+  console.error('class .swiper-table is not found');
+} else {
+  new Swiper('.swiper-table', {
+    enabled: Boolean(window.innerWidth < breakpointSlider),
+    slidesPerView: 'auto',
+    spaceBetween: 5,
+    speed: 1000,
+    allowTouchMove: true,
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      dragSize: 70,
     },
-  },
-});
+  
+    on: {
+      slideChange: function () {
+        console.log('slide changed');
+      },
+    },
+  });
+}
